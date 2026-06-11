0de9765ba2acb5dc442af894753f85c205af6470
zychen <zeyu.chen07@gmail.com>
2026-06-11 16:32:43 +0800

    update

--- scripts/eval_maskdino_orth.sh
+++ scripts/eval_maskdino_orth.sh
@@ -2,7 +2,6 @@ CUDA_VISIBLE_DEVICES=2 \
 python maskdino_pred.py \
   --task orth \
   --data_dir .datasets/intraoral_anno/orth_test/orth_test \
-  --weights output/maskdino_orth_resnet/model_final.pth \
-  --output_dir output/maskdino_orth_resnet_pred \
+  --weights output/maskdino_orth_swin/model_final.pth \
+  --output_dir output/maskdino_orth_swin_pred \
   --eval_splits train test \
-  # DATALOADER.NUM_WORKERS 0
