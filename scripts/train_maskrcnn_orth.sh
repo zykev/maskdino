03d75c48f5970bf3bc45eb53523e2e5687ce51b0
zychen <zeyu.chen07@gmail.com>
2026-07-14 14:03:39 +0800

    update cfgs

--- scripts/train_maskrcnn_orth.sh
+++ scripts/train_maskrcnn_orth.sh
@@ -1,8 +1,9 @@
-CUDA_VISIBLE_DEVICES=4,5 \
+CUDA_VISIBLE_DEVICES=3,4 \
 python maskrcnn_unify.py \
     --task orth \
     --config_file configs/default_maskrcnn_orth_config.yaml \
     --input_dir .datasets/intraoral_anno/orth_0616 \
-    --output_dir output/maskrcnn_orth_0702 \
+    --output_dir output/maskrcnn_orth_0707 \
     --batch_size 8 \
-    --wandb_name maskrcnn_orth_0702
+    --image_subdir tooth_mask \
+    --wandb_name maskrcnn_orth_0707
